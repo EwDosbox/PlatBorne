75b65ffc54f4c9206860844e6eb7597f7eb23487
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2023-09-26 22:30:38 +0200

    Camera stays focused on Player

--- PlatBorne/Assets/Scripts/CameraScript.cs
+++ PlatBorne/Assets/Scripts/CameraScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -100);
     }
 }
