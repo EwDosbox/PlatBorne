ad6884c4ce938f648bb8ed128aee477e59fd123a
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2023-10-20 00:12:40 +0200

    Added CameraScript and started working on it [0/?]

--- PlatBorne/Assets/Scripts/CameraScript.cs
+++ PlatBorne/Assets/Scripts/CameraScript.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
     public GameObject player;
-
-    // Update is called once per frame
+    public Vector2 offset = Vector2.zero;
+    public CompositeCollider2D mapCollider;
     void Update()
     {
-        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -100);
+        bool wasCamInBounds = true;
+        if (math.abs(player.transform.position.x) <= offset.x / 2)
+        {
+            wasCamInBounds = false;
+            if (player.transform.position.x > 0)
+                transform.position = new Vector3(player.transform.position.x - offset.x / 2,
+                    this.transform.position.y);
+            else transform.position = new Vector3(player.transform.position.x + offset.x / 2,
+                this.transform.position.y);
+        }
+        if (math.abs(player.transform.position.y) >= offset.y / 2)
+        {
+            wasCamInBounds = false;
+            if (player.transform.position.y > 0)
+                transform.position = new Vector3(this.transform.position.x,
+                    player.transform.position.y - offset.y / 2);
+            else transform.position = new Vector3(this.transform.position.x,
+                player.transform.position.y + offset.y / 2);
+        }
+        if (wasCamInBounds) 
+        {
+            this.transform.position = new Vector3(
+                player.transform.position.x,
+                player.transform.position.y
+           );
+        }
     }
 }
