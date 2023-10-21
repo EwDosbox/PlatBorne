c5c802daf50018beddf348b78366ff1ce183a4e4
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2023-10-21 12:40:41 +0200

    Camera works final

--- PlatBorne/Assets/Scripts/CameraScript.cs
+++ PlatBorne/Assets/Scripts/CameraScript.cs
@@ -10,29 +10,17 @@ public class CameraScript : MonoBehaviour
     public CompositeCollider2D mapCollider;
     void Update()
     {
-        bool wasCamInBounds = true;
-        if (math.abs(player.transform.position.x) <= offset.x / 2)
-        {
-            wasCamInBounds = false;
-            if (player.transform.position.x > 0)
-                transform.position = new Vector3(player.transform.position.x - offset.x / 2,
-                    this.transform.position.y);
-            else transform.position = new Vector3(player.transform.position.x + offset.x / 2,
-                this.transform.position.y);
-        }
-        if (math.abs(player.transform.position.y) >= offset.y / 2)
-        {
-            wasCamInBounds = false;
-            if (player.transform.position.y > 0)
-                transform.position = new Vector3(this.transform.position.x,
-                    player.transform.position.y - offset.y / 2);
-            else transform.position = new Vector3(this.transform.position.x,
-                player.transform.position.y + offset.y / 2);
-        }
-        if (wasCamInBounds) 
+        if (player.transform.position.y < -60)
         {
             this.transform.position = new Vector3(
-                player.transform.position.x,
+                0,
+                -60  
+                );         
+        }
+        else 
+        {
+            this.transform.position = new Vector3(
+                this.transform.position.x, 
                 player.transform.position.y
            );
         }
