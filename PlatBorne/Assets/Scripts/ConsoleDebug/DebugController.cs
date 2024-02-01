0d5b534b35bcb212083a696f886f018635364399
Kokorotko <prdak05@seznam.cz>
2024-02-01 18:54:07 +0100

    Příkaz a UI na help

--- PlatBorne/Assets/Scripts/ConsoleDebug/DebugController.cs
+++ PlatBorne/Assets/Scripts/ConsoleDebug/DebugController.cs
@@ -6,9 +6,11 @@ using UnityEngine.InputSystem;
 public class DebugController : MonoBehaviour
 {
     bool showConsole;
+    bool showHelp = false;
     string input;
     //commands {
     public static DebugCommand ROSEBUD;
+    public static DebugCommand HELP;
     //commands }
     public List<object> commandList;
     public PlayerHealth playerHealth;
@@ -16,11 +18,28 @@ public class DebugController : MonoBehaviour
     {
         showConsole = !showConsole;
     }
-
+    Vector2 scroll;
     private void OnGUI()
     {
         if (!showConsole) { return; }
         float y = 0;
+        if (showHelp)
+        {
+            GUI.Box(new Rect(0, y, Screen.width, 100), "");
+            Rect viewport = new Rect(0, 0, Screen.width - 30, 20 * commandList.Count);
+            scroll = GUI.BeginScrollView(new Rect(0, y + 5f, Screen.width, 90), scroll, viewport);
+            for (int i = 0; i < commandList.Count; i++)
+            {
+                DebugCommandBase command = commandList[i] as DebugCommandBase;
+                string label = $"{command.CommandFormat} - {command.CommandDescription}";
+                Rect labelRect = new Rect(5, 20 * i, viewport.width - 100, 20);
+                GUI.Label(labelRect, label);
+            }
+            GUI.EndScrollView();
+            {
+                y += 100;
+            }
+        }
         GUI.Box(new Rect(0, y, Screen.width, 30), "");
         GUI.backgroundColor = new Color(0, 0, 0, 0);
         GUI.SetNextControlName("console");
@@ -35,9 +54,15 @@ public class DebugController : MonoBehaviour
             playerHealth.SetGodMode(true);
         });
 
+        HELP = new DebugCommand("help", "Shows all commands", "help", () =>
+        {
+            showHelp = true;
+        });
+
         commandList = new List<object>
         {
-            ROSEBUD
+            ROSEBUD,
+            HELP
         };
     }
 
