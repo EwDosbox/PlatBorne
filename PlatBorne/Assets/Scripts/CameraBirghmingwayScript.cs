14d948d4100875677da453fd1d1c530c7f8786e5
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2024-03-14 22:10:02 +0100

    stuff
    camera birmingweee
    1.2 => 2.0

--- PlatBorne/Assets/Scripts/CameraBirghmingwayScript.cs
+++ PlatBorne/Assets/Scripts/CameraBirghmingwayScript.cs
@@ -1,67 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraBirghmingwayScript : MonoBehaviour
 {
     [SerializeField] private GameObject hunter;
     [SerializeField] private Camera cam;
-    private Vector2 hT;
+    private Vector2 hP;
+    private Vector2 cP;
+
     private void Update()
     {
-        hT = hunter.transform.position;
-        if (hT.x < -26)
+        hP = hunter.transform.position;
+        cP = hP;
+        if (IsOutside())
         {
-            SetTo(new Vector2(-32, -3));
-            cam.orthographicSize = 4;
+            cam.orthographicSize = 3.8f;
+            //X: -32  -32  
+            //Y: -4     2
+            cP.x = -32;
+            if (cP.y < -4f) cP.y = -4f;
+            else if (cP.y > 2f) cP.y = 2f;
         }
-        else if
-            (
-                hT.y > 5 &&
-                hT.x < -9
-            )
+        else if (IsInMainNave())
         {
-            SetTo(new Vector2(hT.x, 5));
+            cam.orthographicSize = 5;
+            if (cP.x < -17f) cP.x = -17f;
+            else if (cP.x > 1f) cP.x = 1f;
+            if (cP.y > 5f) cP.y = 5f;
+            else if (cP.y < -3f) cP.y = -3f;
         }
-        else if
-            (
-                hT.y > 9 &&
-                hT.x > -9
-            )
+        else if (IsInBellTower())
         {
-            SetTo(new Vector2(-1, hT.y));
-        }
-        else if
-            (
-                hT.x > -25 &&
-                hT.y < -3
-            )
-        {
-            SetTo(new Vector2(hT.x, -3));
+            cam.orthographicSize = 5;
+            //X: -1  1 
+            //Y: 14  62
+            if (cP.x < -1f) cP.x = -1f;
+            else if (cP.x > 1f) cP.x = 1f;
+            if (cP.y > 62f) cP.y = 62f;
+            else if (cP.y < 14f) cP.y = 14f;
         }
         else
         {
-            SetToHunter();
-            cam.orthographicSize = 5;
-        }
-    }
-    private void SetToHunter()
-    {
-        this.transform.position = new Vector3(
-            hunter.transform.position.x,
-            hunter.transform.position.y,
-            -25
-       );
-    }
-    private void SetTo(Vector2 position)
-    {
+            transform.position = new Vector3(hP.x, hP.y, -25);
 
-        this.transform.position = new Vector3(
-            position.x,
-            position.y,
-            -25
-       );
+        }
+        cam.transform.position = new Vector3(cP.x, cP.y, -25);
+    }
+    private bool IsOutside()
+    {
+        return hP.x < -25;
+    }
+    private bool IsInMainNave()
+    {
+        return hP.x > -25 && hP.y < 9;
+    }
+    private bool IsInBellTower()
+    {
+        return hP.x > -10;
     }
 }
