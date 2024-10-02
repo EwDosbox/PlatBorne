cc32e3070bf2af42f66caf8c6eb6c85b4147e797
Kokorotko <prdak05@seznam.cz>
2024-10-02 16:32:12 +0200

    Console Update

--- PlatBorne/Assets/Scripts/ConsoleDebug/DebugController.cs
+++ PlatBorne/Assets/Scripts/ConsoleDebug/DebugController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -15,7 +16,8 @@ public class DebugController : MonoBehaviour
     {
         get { return showConsole; } //only for reading
     }
-    //commands {
+
+    // Commands
     public static DebugCommand<bool> ROSEBUD;
     public static DebugCommand HELP;
     public static DebugCommand<string> KILL_BOSS;
@@ -23,28 +25,38 @@ public class DebugController : MonoBehaviour
     public static DebugCommand<bool> PUSSYMODE;
     public static DebugCommand<string> TELEPORT;
     public static DebugCommand RESET_PREFS;
-    public static DebugCommand<bool> DASH; 
-    //commands }
+    public static DebugCommand<bool> DASH;
+
+    // Scripts
+    PlayerHealth playerHealth;
+    PlayerInputScript playerInputScript;
+    Bossfight bossfight;
+    Saves save;
+    Mole_Bossfight moleBossfight;
+
+    // Other
+    string feedbackMessage = "";
+    private Coroutine clearFeedbackCoroutine; // Store reference to the coroutine for clearing feedback
     public List<object> commandListLevel;
     public List<object> commandListBoss;
-    public PlayerHealth playerHealth;
-    public PlayerInputScript playerInputScript;
-    public Bossfight bossfight;
-    public Saves save;
-    public Mole_Bossfight moleBossfight;
     public float consoleLevelHeight = 95;
     public float consoleBossHeight = 170;
+    public float y;
+
     public void OnToggleConsole(InputValue value)
     {
         showConsole = !showConsole;
         input = "";
         PlayerInputScript.CanMove = !PlayerInputScript.CanMove;
     }
+
     Vector2 scroll;
+
     private void OnGUI()
     {
         if (!showConsole) { return; }
-        float y = 0;
+        y = 0;
+
         if (showHelp)
         {
             if (SceneManager.GetActiveScene().name == "LevelLondon" || SceneManager.GetActiveScene().name == "LevelBirmingham")
@@ -82,29 +94,46 @@ public class DebugController : MonoBehaviour
                 }
             }
         }
+
         GUI.Box(new Rect(0, y, Screen.width, 30), "");
         GUI.backgroundColor = new Color(0, 0, 0, 0);
         GUI.SetNextControlName("console");
         input = GUI.TextField(new Rect(10f, y + 5f, Screen.width - 20f, 20f), input);
         GUI.FocusControl("console");
+        GUI.Label(new Rect(10f, y + 30f, Screen.width - 20f, 20f), feedbackMessage); // Display feedback message
     }
 
     private void Awake()
     {
-        ROSEBUD = new DebugCommand<bool>("rosebud", "Enables GodMode", "rosebud", (x) =>
+        // Find scripts
+        if (SceneManager.GetActiveScene().name == "MoleBoss")
+        {
+            moleBossfight = FindObjectOfType<Mole_Bossfight>();
+            playerHealth = FindObjectOfType<PlayerHealth>();
+        }
+        else if (SceneManager.GetActiveScene().name == "LevelBoss")
+        {
+            bossfight = FindObjectOfType<Bossfight>();
+            playerHealth = FindObjectOfType<PlayerHealth>();
+        }
+
+        playerInputScript = FindObjectOfType<PlayerInputScript>();
+        save = FindObjectOfType<Saves>();
+
+        ROSEBUD = new DebugCommand<bool>("rosebud", "Enables GodMode", "rosebud <true/false>", (x) =>
         {
             if (x)
             {
                 if (playerHealth != null) playerHealth.GodMode = true;
-                PlayerPrefs.SetInt("GodMode", 1);
-                PlayerPrefs.Save();
+                PlayerPrefs.SetInt("GodMode", 69);
             }
             else
             {
                 if (playerHealth != null) playerHealth.GodMode = false;
                 PlayerPrefs.DeleteKey("GodMode");
-                PlayerPrefs.Save();
             }
+            CommandCorrectAnswer();
+            PlayerPrefs.Save();
         });
 
         HELP = new DebugCommand("help", "Shows all commands", "help", () =>
@@ -112,51 +141,76 @@ public class DebugController : MonoBehaviour
             showHelp = true;
         });
 
-        KILL_BOSS = new DebugCommand<string>("kill_boss", "Kills boss", "kill_boss <value>", (x) =>
+        KILL_BOSS = new DebugCommand<string>("kill_boss", "Kills boss", "kill_boss", (x) =>
         {
-            if (x == "brecus" && bossfight != null) StartCoroutine(bossfight.BossDeath());
-            if (x == "mole") StartCoroutine(moleBossfight.BossDeath());
+            if (SceneManager.GetActiveScene().name == "LevelBoss" && bossfight != null)
+            {
+                StartCoroutine(bossfight.BossDeath());
+                CommandCorrectAnswer();
+            }
+            if (SceneManager.GetActiveScene().name == "LevelMole" && moleBossfight != null)
+            {
+                StartCoroutine(moleBossfight.BossDeath());
+                CommandCorrectAnswer();
+            }
         });
 
         PLAYER_HEAL = new DebugCommand("player_heal", "Heals Hunter to full HP", "player_heal", () =>
         {
-            if (playerHealth != null) playerHealth.PlayerHP = 3; 
+            if (playerHealth != null)
+            {
+                playerHealth.PlayerHP = 3;
+                CommandCorrectAnswer();
+            }
         });
 
-        PUSSYMODE = new DebugCommand<bool>("pussymode", "Enables/Disables pussy mode", "pussymode <value>", (x) =>
+        PUSSYMODE = new DebugCommand<bool>("pussymode", "Enables/Disables pussy mode", "pussymode <true/false>", (x) =>
         {
             if (x)
             {
                 PlayerPrefs.SetInt("PussyMode", 1);
                 PlayerPrefs.Save();
+                CommandCorrectAnswer();
                 if (playerHealth != null) playerHealth.PussyMode = true;
             }
             else
             {
                 PlayerPrefs.DeleteKey("PussyMode");
                 PlayerPrefs.Save();
+                CommandCorrectAnswer();
                 if (playerHealth != null) playerHealth.PussyMode = false;
             }
         });
 
-
-        TELEPORT = new DebugCommand<string>("teleport", "Teleports Player", "teleport <value>", (x) =>
+        TELEPORT = new DebugCommand<string>("teleport", "Teleports Player", "teleport <london/brecus/birmingham/lunae>", (x) =>
         {
             if (x == "london") SceneManager.LoadScene("LevelLondon");
             else if (x == "brecus") SceneManager.LoadScene("LevelBoss");
             else if (x == "birmingham") SceneManager.LoadScene("LevelBirmingham");
-            else if (x == "mole") SceneManager.LoadScene("LevelMole");
+            else if (x == "lunae") SceneManager.LoadScene("LevelMole");
         });
 
-        RESET_PREFS  = new DebugCommand("reset_prefs", "Resets all PlayerPrefs", "reset_prefs", () =>
+        RESET_PREFS = new DebugCommand("reset_prefs", "Resets all PlayerPrefs", "reset_prefs", () =>
         {
-            if (save != null) save.NewGameSaveReset();
+            if (save != null)
+            {
+                save.NewGameSaveReset();
+                CommandCorrectAnswer();
+            }
         });
 
-        DASH = new DebugCommand<bool>("dash", "Enables/Disables Dash Ability", "dash <value>", (x) =>
+        DASH = new DebugCommand<bool>("dash", "Enables/Disables Dash Ability", "dash <true/false>", (x) =>
         {
-            if (x) playerInputScript.AbilityToDash(true);
-            else playerInputScript.AbilityToDash(false);
+            if (x)
+            {
+                playerInputScript.AbilityToDash(true);
+                CommandCorrectAnswer();
+            }
+            else
+            {
+                playerInputScript.AbilityToDash(false);
+                CommandCorrectAnswer();
+            }
         });
 
         commandListLevel = new List<object>
@@ -166,6 +220,7 @@ public class DebugController : MonoBehaviour
             DASH,
             RESET_PREFS
         };
+
         commandListBoss = new List<object>
         {
             HELP,
@@ -190,9 +245,10 @@ public class DebugController : MonoBehaviour
 
     private void HandleInput()
     {
+        string[] properties = input.Split(' ');
+
         if (SceneManager.GetActiveScene().name == "LevelLondon" || SceneManager.GetActiveScene().name == "LevelBirmingham")
         {
-            string[] properties = input.Split(' ');
             for (int i = 0; i < commandListLevel.Count; i++)
             {
                 DebugCommandBase commandBase = commandListLevel[i] as DebugCommandBase;
@@ -216,7 +272,6 @@ public class DebugController : MonoBehaviour
         }
         else
         {
-            string[] properties = input.Split(' ');
             for (int i = 0; i < commandListBoss.Count; i++)
             {
                 DebugCommandBase commandBase = commandListBoss[i] as DebugCommandBase;
@@ -238,5 +293,22 @@ public class DebugController : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void CommandCorrectAnswer()
+    {
+        feedbackMessage = "Command Done";
+        if (clearFeedbackCoroutine != null)
+        {
+            StopCoroutine(clearFeedbackCoroutine);
+        }
+        clearFeedbackCoroutine = StartCoroutine(ClearFeedbackMessageAfterDelay());
+    }
+
+    private IEnumerator ClearFeedbackMessageAfterDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        feedbackMessage = "";                 
+        clearFeedbackCoroutine = null;        
     }
 }
